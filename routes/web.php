73cf68070692c3907012ea52771e7ea5be6c3ff0
adnane el yazidi <adanyazi22@outlook.com>
2026-06-02 10:12:05 +0100

    feat: add public web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\QuoteRequestController;
+use App\Http\Controllers\ServiceRequestController;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::get('/demande-devis', [QuoteRequestController::class, 'create'])->name('quote.create');
+Route::post('/demande-devis', [QuoteRequestController::class, 'store'])->name('quote.store');
+
+Route::get('/demande-service', [ServiceRequestController::class, 'create'])->name('service.create');
+Route::post('/demande-service', [ServiceRequestController::class, 'store'])->name('service.store');
+
+Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
